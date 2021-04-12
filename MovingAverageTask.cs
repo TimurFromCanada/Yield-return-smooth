9d02bdb75235fa4d535f414b4d104524a7adab58
timurfromcanada <timurfromcanada@gmail.com>
2021-04-12 21:00:35 +0500

    Implemented MovingAverageTask.cs

--- MovingAverageTask.cs
+++ MovingAverageTask.cs
@@ -2,12 +2,31 @@ using System.Collections.Generic;
 
 namespace yield
 {
-	public static class MovingAverageTask
-	{
-		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
-		{
-			//Fix me!
-			return data;
-		}
-	}
+    public static class MovingAverageTask
+    {
+        public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var average = 0.0;
+            Queue<double> queue = new Queue<double>();
+
+            foreach (var e in data)
+            {
+                queue.Enqueue(e.OriginalY);
+                average += e.OriginalY;
+
+                if (windowWidth >= queue.Count)
+                {
+                    e.AvgSmoothedY = average / queue.Count;
+                }
+                    
+                else
+                {
+                    average -= queue.Dequeue();
+                    e.AvgSmoothedY = average / queue.Count;
+                }
+
+                yield return e;
+            }
+        }
+    }
 }
