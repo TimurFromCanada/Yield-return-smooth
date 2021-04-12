4187f99676bebbaa7bb7e166e507d51b5500dd68
timurfromcanada <timurfromcanada@gmail.com>
2021-04-12 21:02:13 +0500

    Implemented MovingMaxTask.cs

--- MovingMaxTask.cs
+++ MovingMaxTask.cs
@@ -1,16 +1,37 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace yield
 {
+    public static class MovingMaxTask
+    {
+        public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var points = new LinkedList<double>();
+            var xPoints = new Queue<double>();
 
-	public static class MovingMaxTask
-	{
-		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-		{
-			//Fix me!
-			return data;
-		}
-	}
+            foreach (var point in data)
+            {
+                xPoints.Enqueue(point.X);
+
+                if (xPoints.Count > windowWidth && points.First.Value <= xPoints.Dequeue())
+                {
+                    points.RemoveFirst();
+                    points.RemoveFirst();
+                }
+
+                while (points.Count != 0 && points.Last.Value < point.OriginalY)
+                {
+                    points.RemoveLast();
+                    points.RemoveLast();
+                }
+
+                points.AddLast(point.X);
+                points.AddLast(point.OriginalY);
+                point.MaxY = points.First.Next.Value;
+
+                yield return point;
+            }
+        }
+    }
 }
+
