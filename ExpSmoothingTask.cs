3d3e3536079b21ed73037212e5124a25f84754ab
timurfromcanada <timurfromcanada@gmail.com>
2021-04-12 20:59:17 +0500

    Implemented ExpSmoothingTask.cs

--- ExpSmoothingTask.cs
+++ ExpSmoothingTask.cs
@@ -6,8 +6,29 @@ namespace yield
 	{
 		public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 		{
-			//Fix me!
-			return data;
+			var e = data.GetEnumerator();
+            double middle;
+
+            if (e.MoveNext())
+            {
+				middle = e.Current.ExpSmoothedY = e.Current.OriginalY;
+			}
+				
+			else
+            {
+				yield break;
+            }
+				
+			yield return e.Current;
+
+			while (e.MoveNext())
+			{
+				e.Current.ExpSmoothedY = alpha * e.Current.OriginalY + (1 - alpha) * middle;
+				yield return e.Current;
+				middle = e.Current.ExpSmoothedY;
+			}
 		}
 	}
 }
+
+
